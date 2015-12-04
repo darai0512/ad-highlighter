2671d3508cb84af4aa56a751b3e974e77e0e1ec8
normany <normany@yahoo-inc.com>
2015-12-05 00:17:31 +0800

    Add RESTful API and polish .gitignore

---
+++ app.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongorest import MongoRest
+from flask.ext.mongorest.views import ResourceView
+from flask.ext.mongorest.resources import Resource
+from flask.ext.mongorest import methods
+
+app = Flask(__name__)
+
+app.config.update(
+    MONGODB_HOST = 'localhost',
+    MONGODB_PORT = '27017',
+    MONGODB_DB = 'ad-highlighter',
+)
+
+db = MongoEngine(app)
+api = MongoRest(app)
+
+class Ad(db.Document):
+    url = db.StringField(max_length=120, required=True)
+    ad_url = db.StringField(max_length=120, required=True)
+    dom_info = db.StringField(max_length=120)
+    image = db.StringField()
+    feature = db.StringField()
+
+class AdResource(Resource):
+    document = Ad
+    related_resources = {}
+    filters = {}
+    rename_fields = {}
+
+@api.register(name='ads', url='/ads/')
+class AdView(ResourceView):
+    resource = AdResource
+    methods = [methods.Create, methods.Update, methods.Fetch, methods.List]
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
