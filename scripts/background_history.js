022f71b607509f561ba1ad5608d8fe1cc20421f8
Bryan Cheng <cycheng@yahoo-inc.com>
2015-12-05 23:03:17 +0800

    20151205

--- scripts/background_history.js
+++ scripts/background_history.js
@@ -1,13 +1,52 @@
 console.log('chrome backgroudn history api');
 
-var yahooDomain = [
-    "tw.sports.yahoo.com",
-    "tw.money.yahoo.com",
-    "tw.movies.yahoo.com"
-]
 
-chrome.history.search({text: 'yahoo', maxResults: 10}, function(data) {
-    data.forEach(function(page) {
-        console.log(page.url);
-    });
-});
+var yahooDomain = [
+{
+	'sports': "tw.sports.yahoo.com",
+    "finance": "tw.money.yahoo.com",
+    "movies": "tw.movies.yahoo.com",
+    "celebrity": "tw.celebrity.yahoo.com",
+    "music": "yahoo.streetvoice.com",
+    "tech": "tw.tech.yahoo.com",
+    "autos": "tw.autos.yahoo.com",
+    "food": "yahoo.gomaji.com"
+},{
+    "sports": "sports.yahoo.co.jp",
+    "finance": "finance.yahoo.co.jp",
+    "autos": "carview.yahoo.co.jp"
+}]
+
+var userDomainRepresent = {
+	"sports": 0,
+    "finance": 0,
+    "movies": 0,
+    "celebrity": 0,
+    "music": 0,
+    "tech": 0,
+    "autos": 0,
+    "food": 0
+}
+
+for(intl in yahooDomain) {
+	for(type in yahooDomain[intl]) {
+		console.log("type", type);						// sports
+		console.log("site", yahooDomain[intl][type]);	// tw.sports.yahoo.com
+		$.when(saerchHistoryCounting(type, yahooDomain[intl][type]));
+	}
+}
+
+console.log("user history score = ", userDomainRepresent);
+
+function saerchHistoryCounting(type, domain) {
+    var d = new $.Deferred();
+	chrome.history.search({text: domain, maxResults: 100}, function(data) {
+	    data.forEach(function(page) {
+	        //console.log(page.url);
+	        userDomainRepresent[type] = userDomainRepresent[type] + 1;
+	    });
+	});
+
+    d.resolve();
+    return d;
+}
--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -3,27 +3,143 @@
 	$(document).ready(function() {
 		console.log('ad-highlighter console log');
 
+		var borderStyleHL = "3px solid red";
+		var adsImageWidth = "60px";
+		var adsImageHeight = "60px";
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		var localStorageWidth = localStorage.adsAverageX ? parseInt(localStorage.adsAverageX) : 0;
+		var localStorageHeight = localStorage.adsAverageY ? parseInt(localStorage.adsAverageY) : 0;
+
+		var targetDomainList = [
+			{ domain: 'mobile01.com', pattern: 'adredir'},
+			{ domain: 'gamer.com.tw', pattern: 'adcounter'},
+			{ domain: 'japantoday.com', pattern: 'ads.gplusmedia'}
+		];
+
 		// hotspot
     	var hotspot_container = document.createElement("div");
-    	$(hotspot_container).addClass("hotspot_container");
-    	$(hotspot_container).css({
-	    	"width": "100%",
-	    	"height": "1px"
-	    });
+    	// addClass via top, bottom, left, right
+    	$(hotspot_container).addClass("hotspot_container").addClass("top");
 
-	    // ads
-	    var ads = document.createElement("div");
-    	$(ads).addClass("ads");
-    	$(ads).text("this is one ads content");
-    	$(hotspot_container).append(ads);
+		var cloneUrlAry = [];
+		var cloneImgAry = [];
 
-		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-			if(request.status == '1') {
-				$("body").before(hotspot_container);
+		console.log("$(window).height()", $(window).height());
+		console.log("$(window).width()", $(window).width());
+		console.log("localStorageWidth", localStorageWidth);
+		console.log("localStorageHeight", localStorageHeight);
 
-				$(hotspot_container).animate({ height: '220px' }, 'slow');
+		// highlight ad which contains `adcounter.php` in href
+		//makrAdsHL("adcounter.php");
+
+
+		$('a').click(function() {
+			for(x in targetDomainList) {
+				console.log('check target ads in domain list');
+				if ( window.location.href.indexOf(targetDomainList[x].domain) != -1 && $(this).attr('href').indexOf(targetDomainList[x].pattern) > -1) {
+					console.log("get targer ads match in domain list");
+					$(this).parent().css("border", borderStyleHL);
+
+					var adPos = $(this).parent().position();
+
+					// adImg
+					var adImg = $(this).find("img").clone();
+					$(adImg).css({"width": adsImageWidth, "height": adsImageHeight});
+
+					cloneUrlAry.push($(this).attr("href"));
+					cloneImgAry.push(adImg);
+
+					console.log("current url = " + window.location.href);
+					console.log("DOM url = " + $(this).attr("href"));
+					console.log("DOM class = " + $(this).parent().attr("class"));
+					console.log("DOM id = " + $(this).parent().attr("id"));
+					console.log("DOM adPos left = " + adPos.left);
+					console.log("DOM adPos top = " + adPos.top);
+
+					// set localstorage
+					localStorageWidth = localStorage["adsAverageX"] = (localStorageWidth + adPos.left)/2;
+					localStorageHeight = localStorage["adsAverageY"] = (localStorageHeight + adPos.top)/2;
+
+					console.log("localStorageWidth", localStorageWidth);
+					console.log("localStorageHeight", localStorageHeight);
+					// change hotspot region
+
+					countHotSpotPosition();
+
+					/*
+					$.ajax({
+						method: "POST",
+					  	url: "some.php",
+					  	data: { name: "John", location: "Boston" }
+					})
+					.done(function( msg ) {
+						console.log( "Data Saved: " + msg );
+					});
+					*/
+
+				}
 			}
 		});
+
+	    // ads hotspot region
+		chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+			if(request.status == '1') {
+				$(hotspot_container).text("");
+				$("body").before(hotspot_container);
+				if (cloneUrlAry.length === 0) {
+					// array is empty
+					var emptyText = document.createElement("div");
+					$(emptyText).addClass("hotspotText").text("No more Ads highlighting");
+					$(hotspot_container).append(emptyText);
+				} else {
+					// update ads
+					for(key in cloneUrlAry) {
+			    		var adsContainer = document.createElement("div");
+			    		var adsLeft = document.createElement("div");
+			    		var adsRight = document.createElement("div");
+
+			    		$(adsLeft).addClass("adsLeft");
+			    		$(adsRight).addClass("adsRight");
+
+			    		$(adsContainer).addClass("adsContainer").append(adsLeft).append(adsRight);
+			    		$(adsLeft).append(cloneImgAry[key]);
+			    		$(adsRight).text("this is one ads content");
+
+			    		$(hotspot_container).append(adsContainer);
+					}
+				}
+				$(hotspot_container).show();
+			}
+		});
+
+		$("body").click(function(){
+			$(hotspot_container).hide();
+		});
+
+		function makrAdsHL(containsText) {
+			$('a[href*="' + containsText + '"]').closest("div").css("border", borderStyleHL);
+		}
+
+		function countHotSpotPosition() {
+			$(hotspot_container).removeClass("top").removeClass("bottom").removeClass("left").removeClass("right");
+			if (localStorageHeight/windowHeight > 0.67) {
+				// bottom
+				$(hotspot_container).addClass("bottom");
+			} else if (localStorageHeight/windowHeight < 0.33) {
+				// top
+				$(hotspot_container).addClass("top");
+			} else if (localStorageWidth/windowWidth > 0.67) {
+				// right
+				$(hotspot_container).addClass("right");
+			} else if (localStorageWidth/windowWidth < 0.33) {
+				// left
+				$(hotspot_container).addClass("left");
+			} else {
+				// default top
+				$(hotspot_container).addClass("top");
+			}
+		}
 
 	});
 
