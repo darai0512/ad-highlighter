347314e13483c9b0696adf72484b12d49476fd10
Bryan Cheng <cycheng@yahoo-inc.com>
2015-12-13 00:29:15 +0800

    added ajax method

--- extension/scripts/contentscript.js
+++ extension/scripts/contentscript.js
@@ -16,10 +16,12 @@
 	$(document).ready(function() {
 		console.log('ad-highlighter console log');
 
+		// configs
 		var borderStyleHL = "3px solid red";
 		var adsImageWidth = "60px";
 		var adsImageHeight = "60px";
-		var scoreThreshold = 8;
+		var insertScoreThreshold = 8;
+		var deleteScoreThreshold = 8;
 		var windowWidth = $(window).width();
 		var windowHeight = $(window).height();
 		var localStorageWidth = localStorage.adsAverageX ? parseInt(localStorage.adsAverageX) : 0;
@@ -30,17 +32,18 @@
 			{ domain: 'japantoday.com', pattern: 'ads.gplusmedia'},
 			{ domain: 'search.yahoo.com', pattern: 'r.msn.com'}
 		];
+		var backendDomain = "http://ec2-52-192-27-88.ap-northeast-1.compute.amazonaws.com:5000";
+		var actionType = ['insert', 'delete'];
 		var mockScore = {autos: 0, celebrity: 2, finance: 0, food: 2, movies: 0, music: 2, sports: 0, tech: 0};
+		var mockImage = "https://s.yimg.com/cv/ae/tw/audience/111130/374x210x4chkuyzx.jpg";
 
 		$.when(getUserScore()).then(function(score){
 			var userScore = score;
 			console.log("userScore", userScore);
-			console.log("diff = ", diffScore(userScore, mockScore));
-
+			console.log("diff = ", diffScore(userScore, mockScore, insertScoreThreshold));
 
 			// hotspot
 	    	var hotspot_container = document.createElement("div");
-	    	// addClass via top, bottom, left, right
 	    	$(hotspot_container).addClass("hotspot_container").addClass("top");
 
 			var cloneUrlAry = [];
@@ -52,23 +55,70 @@
 			console.log("localStorageWidth", localStorageWidth);
 			console.log("localStorageHeight", localStorageHeight);
 
-			// highlight ad which contains `adcounter.php` in href
-			//makrAdsHL("adcounter.php");
+			// ads highlighting
+			// check a href link in current page
+			$('a').filter(function(index) {
+				for(x in targetDomainList) {
+					var currUrl = window.location.href;
+					var currHref = (typeof $(this).attr('href') === 'undefined') ? 'undefined' : $(this).attr('href');
+					//console.log("currUrl", currUrl);
+					//console.log("currHref", currHref);
+					if ( currUrl.indexOf(targetDomainList[x].domain) !== -1 && currHref.indexOf(targetDomainList[x].pattern) > -1) {
+						// get href
+						//console.log("$(this).attr('href')", $(this).attr('href'));
 
-			// get all ads info from backend DB
-			/*
-			$.ajax({
-				method: "POST",
-			  	url: "some.php",
-			  	data: {
-			  		currentUrl: "currentUrl"
-			  	}
-			})
-			.done(function(msg) {
-				console.log( "Data Saved: " + msg );
+						// get each ads info from backend DB
+						/*
+						$.ajax({
+							method: "GET",
+						  	url: backendDomain + "/ads/?ad_url__exact=" + $(this).attr('href')
+						})
+						.done(function(data) {
+							for(row in data.data) {
+								var targetAds = data.data[row];
+								// click
+								if (targetAds.action == 'click') {
+									if( diffScore(userScore, targetAds.feature, insertScoreThreshold) ) {
+										makrAdsHL(targetAds.ad_url);
+									}
+								// delete
+								} else if (targetAds.action == 'delete') {
+									if (diffScore(userScore, targetAds.feature, deleteScoreThreshold) ) {
+										removeAdsHL(targetAds.ad_url);
+									}
+								}
+							}
+						});
+						*/
+					}
+				}
 			});
-			*/
 
+			// mock ajax get
+			$.ajax({
+				method: "GET",
+			  	url: backendDomain + "/ads/?ad_url__exact=" + mockImage
+			})
+			.done(function(data) {
+				//console.log( "data " ,data );
+				for(row in data.data) {
+					var targetAds = data.data[row];
+					// click
+					if (targetAds.action == 'click') {
+						if( diffScore(userScore, targetAds.feature, insertScoreThreshold) ) {
+							makrAdsHL(targetAds.ad_url);
+						}
+					// delete
+					} else if (targetAds.action == 'delete') {
+						if (diffScore(userScore, targetAds.feature, deleteScoreThreshold) ) {
+							removeAdsHL(targetAds.ad_url);
+						}
+
+					}
+				}
+			});
+
+			// ads click handling
 			$('a').click(function() {
 				for(x in targetDomainList) {
 					console.log('check target ads in domain list');
@@ -76,6 +126,11 @@
 						console.log("get targer ads match in domain list");
 						$(this).parent().css("border", borderStyleHL);
 
+						var pageUrl = window.location.href;
+						var adUrl = $(this).attr("href");
+						var adImg = $(this).find('img').attr('src');
+						var adText = $(this).text();
+						var feature = userScore;
 						var adPos = $(this).parent().position();
 						var adText = $(this).text() ? $(this).text() : "this is an image ads";
 						var adImg = $(this).find("img").clone();
@@ -85,12 +140,16 @@
 						cloneImgAry.push(adImg);
 						cloneTextAry.push(adText);
 
-						console.log("current url = " + window.location.href);
-						console.log("DOM url = " + $(this).attr("href"));
-						console.log("DOM class = " + $(this).parent().attr("class"));
-						console.log("DOM id = " + $(this).parent().attr("id"));
-						console.log("DOM adPos left = " + adPos.left);
-						console.log("DOM adPos top = " + adPos.top);
+						// console
+						console.log("pageUrl ", pageUrl);
+						console.log("adUrl ", adUrl);
+						console.log("adImg ", adImg);
+						console.log("adText ", adText);
+						console.log("adPos left ", adPos.left);
+						console.log("adPos top ", adPos.top);
+						console.log("userScore ", userScore);
+						console.log("DOM class ", $(this).parent().attr("class"));
+						console.log("DOM id ", $(this).parent().attr("id"));
 
 						// set localstorage
 						localStorageWidth = localStorage["adsAverageX"] = (localStorageWidth + adPos.left)/2;
@@ -98,8 +157,8 @@
 
 						console.log("localStorageWidth", localStorageWidth);
 						console.log("localStorageHeight", localStorageHeight);
-						// change hotspot region
 
+						// change hotspot region
 						countHotSpotPosition(hotspot_container);
 
 						// send click ads info to DB
@@ -174,6 +233,10 @@
 			$('a[href*="' + containsText + '"]').closest("div").css("border", borderStyleHL);
 		}
 
+		function removeAdsHL(containsText) {
+			$('a[href*="' + containsText + '"]').remove();
+		}
+
 		function countHotSpotPosition(hotspot_container) {
 			$(hotspot_container).removeClass("top").removeClass("bottom").removeClass("left").removeClass("right");
 			if (localStorageHeight/windowHeight > 0.67) {
@@ -195,7 +258,7 @@
 		}
 
 		// edit distance counting
-		function diffScore(userScore, mockScore) {
+		function diffScore(userScore, mockScore, scoreThreshold) {
 			var sum = 0;
 			//console.log("diff s1", userScore);
 			//console.log("diff s2", mockScore);
